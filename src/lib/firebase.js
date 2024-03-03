1ed46c9315f8f095edd70bebd37cd9853cee70a1
Amritesh verma <amriteshverma129@gmail.com>
2024-03-03 16:22:43 +0530

    created new project on firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBoyRRn2_f-d2d09OzBjml_paS-C7RrVE8",
-  authDomain: "netflixopenai-252d2.firebaseapp.com",
-  projectId: "netflixopenai-252d2",
-  storageBucket: "netflixopenai-252d2.appspot.com",
-  messagingSenderId: "30614124330",
-  appId: "1:30614124330:web:acd75e9d65f6a0630e5053",
-  measurementId: "G-DX4G2K25T6",
+  apiKey: "AIzaSyAR2CgzLUbbSYbaktgSuV8UDfDbWPna8pk",
+  authDomain: "netflixgpt2-594fb.firebaseapp.com",
+  projectId: "netflixgpt2-594fb",
+  storageBucket: "netflixgpt2-594fb.appspot.com",
+  messagingSenderId: "740957477787",
+  appId: "1:740957477787:web:3b6950be9976202bcb9f3e",
+  measurementId: "G-PMZWX3BC5V",
 };
 
 // Initialize Firebase
